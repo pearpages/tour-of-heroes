fcd3abad81d69d880291fbbd35e980ddf09e243d
pearpages <pere@soms.cat>
2016-04-15 18:53:11 +0200

    template

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -9,20 +9,7 @@ import { HeroService } from './hero.service';
 @Component({
 	selector: 'my-heroes',
   directives: [HeroDetailComponent],
-	template: `
-	<h2>My Heroes</h2>
-	<ul class="heroes">
-		<li *ngFor="#hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
-			<span class="badge">{{hero.id}}</span> {{hero.name}}
-		</li>
-	</ul>
-  <div *ngIf="selectedHero">
-  <h2>
-    {{selectedHero.name | uppercase}} is my hero
-  </h2>
-  <button (click)="gotoDetail()">View Details</button>
-</div>
-  `,
+	templateUrl: 'app/heroes.component.html',
 	styleUrls: ['app/heroes.component.css']
 })
 
